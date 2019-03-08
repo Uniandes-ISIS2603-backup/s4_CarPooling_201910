9642f5305bb5437a063ea639736e9bd6768f4a40
juanfelipegc <jf.garcia@uniandes.edu.co>
2019-03-08 13:00:33 -0500

    Implementación de los cuerpos de los métodos en Usuario Resource. Todavía falta documentar.

--- s4_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/resources/UsuarioResource.java
+++ s4_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/resources/UsuarioResource.java
@@ -6,9 +6,14 @@
 package co.edu.uniandes.csw.carpooling.resources;
 
 import co.edu.uniandes.csw.carpooling.dtos.UsuarioDTO;
+import co.edu.uniandes.csw.carpooling.ejb.UsuarioLogic;
+import co.edu.uniandes.csw.carpooling.entities.UsuarioEntity;
+import co.edu.uniandes.csw.carpooling.exceptions.BusinessLogicException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.*;
 import javax.ws.rs.Path;
@@ -16,7 +21,7 @@ import javax.ws.rs.Produces;
 
 /**
  *
- * @author estudiante
+ * @author 
  */
 
 @Path("usuarios")
@@ -24,36 +29,63 @@ import javax.ws.rs.Produces;
 @Consumes("application/json")
 @RequestScoped
 public class UsuarioResource {
+    
+    //Isa, falta documentar cada método, poner tu nombre de usuario en el autor, y revisar que la implementación funcione.
+    //Las excepciones las manejé como en los ejemplos, la clase resource también lanza las excepciones de lógica. Corrige y organiza a tu gusto.
 
     private static final Logger LOGGER = Logger.getLogger(UsuarioResource.class.getName());
     
+    @Inject
+    private UsuarioLogic logic;
    
     @GET
     public List getUsuarios()
     {
-        return null;
+        List<UsuarioDTO> usuarios = listEntityToDTO(logic.getUsuarios());
+        return usuarios;
     }
     @GET
     @Path("{username: [a-zA-Z][a-zA-Z]*}}")
     public UsuarioDTO getUsuario(@PathParam("username")String username)
     {
-        return null;
+        UsuarioEntity usuario = logic.getUsuario(username);
+        if (usuario == null) {
+            throw new WebApplicationException("El usuario con nombre: " + username + "no existe", 404);
+        }
+        return new UsuarioDTO(usuario);
     }
     @POST
-    public UsuarioDTO createUsuario(UsuarioDTO usuario){
-        return usuario;
+    public UsuarioDTO createUsuario(UsuarioDTO usuario) throws BusinessLogicException{
+        UsuarioEntity entity = usuario.toEntity();
+        entity = logic.create(entity);
+        return new UsuarioDTO(entity);
     }
     @PUT
     @Path("{username: [a-zA-Z][a-zA-Z]*}}")
-    public UsuarioDTO updateUsuario(@PathParam("username")String username, UsuarioDTO usuario)
+    public UsuarioDTO updateUsuario(@PathParam("username")String username, UsuarioDTO usuario) throws BusinessLogicException
     {
-        return usuario;
+        UsuarioEntity entity = usuario.toEntity();
+        entity = logic.updateUsuario(username, entity);
+        return new UsuarioDTO(entity);
     }
     @DELETE
     @Path("{username: [a-zA-Z][a-zA-Z]*}}")
-    public void deleteUsuario(@PathParam("username")String username)
+    public void deleteUsuario(@PathParam("username")String username) throws BusinessLogicException
     {
-        
+        logic.deleteUsuario(username);
+    }
+    
+    /**
+     * Se utiliza un método para convertir una lista de Entidades a DTOs.
+     * @param Usuario
+     * @return una lista de DTOs.
+     */
+    private List<UsuarioDTO> listEntityToDTO(List<UsuarioEntity> Usuario) {
+        List<UsuarioDTO> list = new ArrayList<>();
+        for (UsuarioEntity entity : Usuario) {
+            list.add(new UsuarioDTO(entity));
+        }
+        return list;
     }
 
 }
