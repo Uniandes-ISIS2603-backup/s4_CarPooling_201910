f5100a780a9173508d4445f902c755e0bb561c41
Isabela <im.sarmiento@uniandes.edu.co>
2019-04-07 12:45:11 -0500

    Correción persistence.xml y archivos IT en el que se cambio el entorno

--- s4_carpooling-api/src/test/java/co/edu/uniandes/csw/carpooling/tests/postman/SeguroIT.java
+++ s4_carpooling-api/src/test/java/co/edu/uniandes/csw/carpooling/tests/postman/SeguroIT.java
@@ -54,7 +54,7 @@ public class SeguroIT {
     @RunAsClient
     public void postman() throws IOException {
         PostmanTestBuilder tp = new PostmanTestBuilder();
-        tp.setTestWithoutLogin(COLLECTION, "Entorno-IT.postman_environment");
+        tp.setTestWithoutLogin(COLLECTION, "Entorno-Colecciones.postman_environment");
         String desiredResult = "0";
         Assert.assertEquals("Error en Iterations de: " + COLLECTION, desiredResult, tp.getIterations_failed());
 
